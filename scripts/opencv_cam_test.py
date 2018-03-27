a4328678c44438cf59f1236b8e0fd31d581bf4ad
Yuhuang Hu <duguyue100@gmail.com>
2018-03-27 14:27:38 +0200

    update opencv cam test

---
+++ scripts/opencv_cam_test.py
@@ -0,0 +1,27 @@
+"""Testing for OpenCV webcam.
+
+Author: Yuhuang Hu
+Email : duguyue100@gmail.com
+"""
+from __future__ import print_function, absolute_import
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
